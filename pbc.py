164893073dfbf4bec066682cbae0f6a2a4d4b812
Robert Rauschen <robert.rauschen@rwth-aachen.de>
2021-04-03 15:35:18 +0200

    adding documentation

--- pbc.py
+++ pbc.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+# toolkit for the use of periodic boundary conditions (pbc)
+
+# define translational symmetry for coordinates (x_size = box size)
+# coordinates outside the box will be shifted back into the box
 def per_x(x, x_size):
     if (x <= -x_size * 0.5):
         return x + x_size
@@ -8,6 +12,8 @@ def per_x(x, x_size):
     else:
         return x
 
+# distance calculation with pbc
+# maximum allowed distance with pbc is half the box length
 def per_d(x1, x2, x_size):
     dx = x2 - x1
     if (dx <= -x_size * 0.5):
